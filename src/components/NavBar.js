3bac9c250ad52623e9436628db8667d7a48e59aa
python <angelaclangat@gmail.com>
2025-05-20 15:58:40 +0300

    Creates Components and Pages

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+//step1: import react
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// step 2: Create NavBar function
+
+function NavBar(){
+    return (
+       <nav class="navbar navbar-expand-sm bg-body-tertiary">
+        `  <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/contact">Contact</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+// step 4: export NavBar
+
+export default NavBar
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+
+    return(
+        //html
+        <div>
+
+            <h1>Home Page</h1>
+            <p>This is my home page</p>
+        </div>
+        
+    )
+}
+
+export default Home;
